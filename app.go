b16fd2b59a8d4924145ed13d46908ff13296c942
Ulan N <ulan17nurym@gmail.com>
2021-09-28 10:49:33 +0500

    refactor: update app

--- app.go
+++ app.go
@@ -3,9 +3,10 @@ package r4r
 import "net/http"
 
 func Use(URL string, middlewares []Middleware, router *Router) {
-	for routeURL, route := range router.routes {
-		(func(routeURL string, route *Route) {
-			http.HandleFunc(URL+routeURL, wrapWithMiddlewares(route.ToHandler(), middlewares))
-		})(routeURL, route)
+	router.URL = URL
+	for _, route := range router.routes {
+		(func(route *Route) {
+			http.HandleFunc(router.URL+route.URL, wrapWithMiddlewares(route.ToHandler(), middlewares))
+		})(route)
 	}
 }
